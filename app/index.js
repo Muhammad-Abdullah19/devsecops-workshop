bf747dcac869c298c808518a10904cead14a1c80
Muhammad-Abdullah19 <asgharabdullah2003@gmail.com>
2026-01-30 16:38:01 +0500

    updated Express version

--- app/index.js
+++ app/index.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('DevSecOps Workshop Working!'));
 app.listen(3000, () => console.log('Running on port 3000'));
+const express = require('express');
+const app = express();
+// The port MUST match what you put in your Dockerfile and Service
+const port = 3000; 
+
+app.get('/', (req, res) => {
+    res.send('DevSecOps Workshop Working!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`App running on http://0.0.0.0:${port}`);
+});
